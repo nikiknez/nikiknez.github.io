2f09b0c72fbd2d89b48c946976a708f37e7911a0
Nikola Knezevic <nikiknez@gmail.com>
2023-03-19 01:10:57 +0100

    change panorama and info

---
+++ cvinfo.js
@@ -0,0 +1,72 @@
+
+function createContact(div) {
+    div.innerHTML = "<h2>Contact</h2>\
+        <h3>Address</h3> <h4>Belgrade, Bulevar Umetnosti 1</h4>\
+        <h3>Email</h3> <h4>nikiknez@gmail.com</h4>";
+}
+
+function createEducation(div) {
+    div.innerHTML = "<h2>Education</h2>\
+        <h5>Master studies [2015. – 2018.]</h5>\
+        <h6>School of Electrical Engineering, University of Belgrade</h6>\
+        <h6>Computer Engineering and Information Theory</h6>\
+        <h5>Bechelor studies [2011. – 2015.]</h5>\
+        <h6>School of Electrical Engineering, University of Belgrade</h6>\
+        <h6>Computer Engineering and Information Theory</h6>\
+        <h5>High School of Electrical Engeneering [2007. – 2011.]</h5>\
+        <h6>Computer technician</h6>";
+}
+
+function createSkills(div) {
+    div.innerHTML = "<h2>Skills</h2>\
+        <h5>Programming languages</h5>\
+        <h6>Java, JavaScript, C, C++, Kotlin, Python, VHDL, x86 assembler</h6>\
+        <h5>Web technologies</h5>\
+        <h6>HTML, CSS, React JS, JSP, JSF, Spring Boot, Node JS, AWS</h6>\
+        <h5>Other skills</h5>\
+        <h6>Concurrent and Distributed Programming, Fundamentals of Digital Electronics,\
+        Computer Architecture and Organization, Multiprocessor Systems</h6>";
+}
+
+function createWorkExperience(div) {
+    div.innerHTML = "<h2>Work Experience</h2>\
+        <h5>Web developer – freelance [February – Jun 2022.]</h4>\
+        <h6>Developing web application for recording and tracking state of fishery (JavaScript, React, Firebase, NoSQL)</h6>\
+        <h5>Android software developer - CodeMeUp [November 2021. – Present]</h4>\
+        <h6>Developing Android TV application (Java, Android)</h6>\
+        <h5>Embedded software developer - RT-RK [Jun 2016. – Jun 2021.]</h4>\
+        <h6>Developing production test software for media reciever devices (C) [Jun – Dec 2016.]</h6>\
+        <h6>Integrating WebKit browser into media reciever platform (C++) [Jan 2017. – May 2018.]</h6>\
+        <h6>Developing Android TV application (Java, Android) [Jun 2018. – Jun 2021.]</h6>";
+}
+
+function createSideProjects(div) {
+    div.innerHTML = "<h2>Side Projects</h2>\
+        <h6>Extracting faces from MPEG video streams (Java, ffmpeg, OpenCV)</h6>\
+        <h6>Same song detection system (Java, Spring Boot) [not finished]</h6>\
+        <h6>2D Simulation of gravitational forces between planets (Android)</h6>\
+        <h6>3D Simulation of the Solar System (C++, OpenGL)</h6>\
+        <h6>Mini games: Java (Swing), C++ (CLI games), Android (2D)</h6>";
+}
+
+function createSchoolProjects(div) {
+    div.innerHTML = "<h2>School Projects</h2>\
+        <h6>Final BSc and MSc thesis – Web platform for detection and visualization of traffic conditions\
+        using internet cameras and mobile devices (Spring Boot, Android, JavaScript, computer vision)</h6>\
+        <h6>Designing and implementing point of sale software for a restaurant (Java, Android)</h6>\
+        <h6>Developing a web application for the music festivals (JSF, SQL, Hibernate)</h6>\
+        <h6>Crossword Puzzle solver (Java, AI)</h6>\
+        <h6>Developing compiler for Mikrojava programming language (Java)</h6>\
+        <h6>Designing RISC processor on FPGA chip (VHDL)</h6>\
+        <h6>Developing a JIT emulator for x86 machine (C)</h6>\
+        <h6>Developing a file system with synchronization (C++)</h6>\
+        <h6>OS Kernel, subsystem for threading management and synchronization (C++)</h6>";
+}
+
+function createPersonalityAndInterests(div) {
+    div.innerHTML = "<h2>Personality and interests</h2>\
+        <h6>Passion for quality and performance</h6>\
+        <h6>Fast learner, open for new technologies and expansion of knowledge</h6>\
+        <h6>Areas of interest: TDD, computer vision, cloud technologies</h6>\
+        <h6>Hobby: Enduro riding, BMX Freestyle, Football, Diving</h6>";
+}
